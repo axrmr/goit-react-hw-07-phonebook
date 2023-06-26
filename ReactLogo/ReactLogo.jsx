bb94d7e052618e18331cfc7f11c8c4f61eeea75a
axrmr <97700676+axrmr@users.noreply.github.com>
2023-06-26 14:33:56 +0000

    Deploying to gh-pages from @ axrmr/goit-react-hw-07-phonebook@969e1a5d0df70410a02b46a2086d7f8a8e3c7681 🚀

---
+++ ReactLogo/ReactLogo.jsx
@@ -0,0 +1,11 @@
+import reactLogo from '/react.svg';
+
+const ReactLogo = () => {
+  return (
+    <a href='https://react.dev' target='_blank'>
+      <img src={reactLogo} className='logo react' alt='React logo' />
+    </a>
+  );
+};
+
+export default ReactLogo;
